a30afd9a1553c9da818082ae227d04f171ddad57
krawczyk071 <krawczyk071@gmail.com>
2023-06-04 20:01:44 +0200

    adding records by API

--- website/api.py
+++ website/api.py
@@ -1,11 +1,38 @@
 from flask import Blueprint, request, jsonify
+from . import db
 from .models import Todo
 
 api = Blueprint('api', __name__)
 
 
-@api.route("/", methods=["GET"])
+@api.route("/", methods=['POST', 'GET'])
 def home():
-    todo_list = Todo.query.all()
+    if request.method == 'POST':
+        name = request.form.get('name')
+        return jsonify({
+            'name': name
+        })
+    else:
+        # todo_list = Todo.query.all()
+        todo_list = []
 
-    return jsonify(todo_list[0].title)
+        for todo in Todo.query.all():
+            x = todo.__dict__
+            x.pop('_sa_instance_state', None)
+            todo_list.append(x)
+
+        print(todo_list)
+        return jsonify(todo_list)
+
+
+@api.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return jsonify({
+        'id': new_todo.id,
+        'title': new_todo.title,
+        'complete': new_todo.complete,
+    })
