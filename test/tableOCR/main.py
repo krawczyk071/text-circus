431be65539f9247bd609964220abda7a7284da1b
krawczyk071 <krawczyk071@gmail.com>
2023-10-02 21:51:52 +0200

    clip to ocr

--- test/tableOCR/main.py
+++ test/tableOCR/main.py
@@ -1,12 +1,3 @@
-main.pyimport OcrToTableTool as ottt
-import TableExtractor as te
-import TableLinesRemover as tlr
-import cv2
-
-path_to_image = "./image/nutrition_table.jpg"
-table_extractor = te.TableExtractor(path_to_image)
-perspective_corrected_image = table_extractor.execute()
-cv2.imshow("perspective_corrected_image", perspective_corrected_image)
 import OcrToTableTool as ottt
 import TableExtractor as te
 import TableLinesRemover as tlr
@@ -22,7 +13,8 @@ lines_remover = tlr.TableLinesRemover(perspective_corrected_image)
 image_without_lines = lines_remover.execute()
 cv2.imshow("image_without_lines", image_without_lines)
 
-ocr_tool = ottt.OcrToTableTool(image_without_lines, perspective_corrected_image)
+ocr_tool = ottt.OcrToTableTool(
+    image_without_lines, perspective_corrected_image)
 ocr_tool.execute()
 
 cv2.waitKey(0)
@@ -32,7 +24,8 @@ lines_remover = tlr.TableLinesRemover(perspective_corrected_image)
 image_without_lines = lines_remover.execute()
 cv2.imshow("image_without_lines", image_without_lines)
 
-ocr_tool = ottt.OcrToTableTool(image_without_lines, perspective_corrected_image)
+ocr_tool = ottt.OcrToTableTool(
+    image_without_lines, perspective_corrected_image)
 ocr_tool.execute()
 
 cv2.waitKey(0)
